e1d99a4fb6c7a6100b0cefdb00f34a7db712f84b
SBC-VN <vnestohr@juno.com>
2019-06-29 21:45:28 -0400

    Updates

---
+++ precache-manifest.67b3488d6fd2d5373275284140488ff4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a04f6665db8bb096ba83eb2aeded0aa",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "1244674b740e7cd9a015",
+    "url": "/clickygame/static/css/main.bc82cb1b.chunk.css"
+  },
+  {
+    "revision": "0994500d6d3d4fbedb55",
+    "url": "/clickygame/static/js/2.8174d921.chunk.js"
+  },
+  {
+    "revision": "1244674b740e7cd9a015",
+    "url": "/clickygame/static/js/main.ed48c62a.chunk.js"
+  },
+  {
+    "revision": "0f0146524616eea72d1b",
+    "url": "/clickygame/static/js/runtime~main.bb8261cb.js"
+  }
+]);
